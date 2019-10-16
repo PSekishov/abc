28592d8f0e3be22e5362fbbfddfce06a806b9ea4
PSekishov <pasha-sveta@yandex.ru>
2019-10-16 16:00:14 +0300

    Создал основные файлы
    index.php bootstrap и тд

---
+++ assets/myscript.js
@@ -0,0 +1,1 @@
+alert('Good job Vlad it\'s work!!!!');
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Index</title>
+</head>
+<body>
+  <!----------- 
+    1 Подключи все файлы с assets
+    2 Установи с сайта bootstrap.com  навигационное меню 
+    3 Задай для body любой цвет
+    4 Ну вот пока и всё !!!
+    ---------->
+
+    <script src="assets/myscript.js"></script>
+</body>
+</html> 
